81291264fa7c576d1a27b60f433d312d1c608f65
asmitghimire3000 <asmitghimire3000@gmail.com>
2026-01-06 10:48:25 +0545

    FUNCTIONAL

--- SIMULATION_CODE.py
+++ SIMULATION_CODE.py
@@ -113,3 +113,120 @@ def multipath_channel(tx_signal, delay, gain):
     rx_signal = rx_signal_full[:len(tx_signal)]
     return rx_signal
 
+def receiver(rx_signal, pilot_positions, num_ofdm_symbols, num_data_symbols=None, fft_size=64, cplen=16):
+    ofdm_symbol_length = fft_size + cplen                                                                   # 80
+    total_samples_needed = num_ofdm_symbols * ofdm_symbol_length                                            # 80*2 = 160
+    # print(total_samples_needed)
+    if len(rx_signal) < total_samples_needed:
+        rx_signal_trimmed = np.concatenate([rx_signal, np.zeros(total_samples_needed - len(rx_signal))])
+    else:
+        rx_signal_trimmed = rx_signal[:total_samples_needed]
+
+
+        # CHANGE TO (2,80)
+        rx_signal_reshaped = rx_signal_trimmed.reshape(num_ofdm_symbols, ofdm_symbol_length)
+        # print(np.shape(rx_signal_reshaped))
+
+        # REMOVE CP
+        rx_symbols_no_cp = rx_signal_reshaped[:, cplen:cplen + fft_size]
+        print("\n\n\n___________RX SYMBOLS WITH NO CP_________\n",np.shape(rx_symbols_no_cp))
+
+        recovered_freq_domain = np.fft.fft(rx_symbols_no_cp, axis=1)
+        # print("\n\n\n_______RECOVERED SYMBOLS_______\n",recovered_freq_domain)
+        print("\n SHAPE \n",np.shape(recovered_freq_domain))
+
+    equalized_symbols = np.zeros_like(recovered_freq_domain)
+    data_positions = np.setdiff1d(np.arange(fft_size), pilot_positions)
+
+
+    for i in range(num_ofdm_symbols):
+        received_pilots = recovered_freq_domain[i, pilot_positions]
+        # print("\n\n\n________RECEIVED PILOTS_________\n",received_pilots)
+
+        transmitted_pilots = (1 + 1j) / np.sqrt(2)
+        
+        # Channel estimation at pilot positions
+        channel_at_pilots = received_pilots / transmitted_pilots
+        # print("\n\n\n__________received_pilots / transmitted_pilots____________\n",channel_at_pilots)
+        all_positions = np.arange(fft_size)
+        
+        # Interpolate real part
+        channel_real = np.interp(all_positions, pilot_positions, 
+                               np.real(channel_at_pilots),
+                               left=np.real(channel_at_pilots[0]),
+                               right=np.real(channel_at_pilots[-1]))
+        # print("\n\n\n__________CHANNEL REAL INTERPOLATION____________\n",channel_real)
+        
+        channel_imag = np.interp(all_positions, pilot_positions,
+                               np.imag(channel_at_pilots),
+                               left=np.imag(channel_at_pilots[0]),
+                               right=np.imag(channel_at_pilots[-1]))
+        # print("\n\n\n__________CHANNEL IMAGINARY INTERPOLATION____________\n",channel_imag)
+
+        
+        # Combine into complex channel estimate
+        channel_estimate = channel_real + 1j * channel_imag
+        
+        # Equalization with regularization
+        equalizer = 1.0 / (channel_estimate)
+        equalized_symbols[i] = recovered_freq_domain[i] * equalizer
+
+        received_data_symbols = equalized_symbols[:, data_positions].flatten()
+
+        if num_data_symbols is not None:
+            received_data_symbols = received_data_symbols[:num_data_symbols]
+    
+    received_bits = qam16_demodulator(received_data_symbols)
+
+    return received_bits
+
+def calc_error(original_bits, received_bits):
+    same = 0
+    length = len(original_bits)
+    for i in range (length):
+        if original_bits[i] != received_bits[i]:
+            print(f"Error at position {i}: original {original_bits[i]}, received {received_bits[i]} \n")
+        else:
+            same += 1
+    print("______________CORRECT BITS_________________\n",same)
+
+    errors = np.sum(original_bits != received_bits)
+    BER = errors / len(original_bits)
+    print("______________BIT ERROR RATE_________________\n", BER * 100)
+
+def main():
+    original_image = 'C:\\Users\\Lenovo\\Desktop\\STUDY\\SIGNAL PROCESSING\\OFDM\\image.png' 
+    bits, w, h = image_to_bits(original_image)
+    # bits = np.random.randint(0, 2, 10000)
+    print("_______________LEN OF BITS_________________\n\n", len(bits))
+
+    # Convert bit string to numpy array of integers
+    bits_array = np.array(list(bits), dtype=int)
+
+    tx_signal, original_symbols, pilot_positions, num_ofdm_symbols = transmitter(bits_array)
+
+    #ADD MULTIPATH AND NOISE
+    #MULTIPATH
+    delay = [0, 3, 8 , 12]  
+    gain = [1, 0.3, 0.2, 0.1]  
+
+    rx_signal = multipath_channel(tx_signal, delay, gain)
+    rx_signal = add_awgn(rx_signal, snr_db=15)  # Add AWGN
+
+    print("\n\n\n________LENGTH OF SIGNAL AFTER PASSING FROM MULTIPATH__________\n",len(rx_signal))
+
+    out_bits= receiver(rx_signal, pilot_positions, num_ofdm_symbols, len(original_symbols), fft_size=64, cplen=16)
+
+    print("\n\n\n________INPUT BITS__________\n",np.array(bits))
+    print("\n\n\n________OUTPUT BITS__________\n",out_bits)
+    print("\n\n\n________OUTPUT BITS LENGHT__________\n",len(out_bits))
+
+    calc_error(np.array([int(b) for b in bits]), out_bits)
+
+    received_image = bits_to_image(out_bits, w, h)
+    received_image.show()
+    received_image.save('reconstructed_image.png')
+
+    pass
+
+main()
